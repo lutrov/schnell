d188ad69fe2942b53064d301080c4059efbcd485
Ivan Lutrov <lutrov@users.noreply.github.com>
2021-03-05 13:56:58 +1100

    Update schnell.php

--- schnell.php
+++ schnell.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: Schnell
-Description: Improves the speed of your site by enabling client side caching with "expire" headers, "cache-control" headers &amp; output compression. Why this plugin name? Schnell means "quick" in German.
+Description: Improves the speed of your site by enabling client side caching with `Expire` headers, `Cache-Control` headers &amp; output compression. Why this plugin name? Schnell means "quick" in German.
 Plugin URI: https://github.com/lutrov/schnell
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 4.0
+Version: 5.0
 */
 
 defined('ABSPATH') || die('Ahem.');
@@ -22,13 +22,13 @@ function schnell_htaccess_file($action) {
 			fclose($fp);
 			$rules = schnell_htaccess_rules();
 			$hash = sprintf('[[%s]]', hash('sha1', $rules));
-			$content = preg_replace('/# BEGIN Schnell Optimisation(.+)# END Schnell Optimisation/isU', $hash, $content, -1, $count);
+			$content = preg_replace('/# BEGIN Schnell Cache(.+)# END Schnell Cache/isU', $hash, $content, -1, $count);
 			switch ($action) {
 				case 'install':
 					if ($count > 0) {
 						$content = trim(str_replace($hash, $rules, $content));
 					} else {
-						$content = sprintf('%s%s%s%s', trim($content), PHP_EOL, PHP_EOL, $rules);
+						$content = sprintf('%s%s%s%s', $rules, PHP_EOL, PHP_EOL, trim($content));
 					}
 					break;
 				case 'uninstall':
@@ -46,116 +46,88 @@ function schnell_htaccess_file($action) {
 }
 
 //
-// Build .htaccess rules.
+// Build .htaccess expiry & compression rules.
+// Cache expiry is 1 year for images/video, 1 month for JS/fonts/PDF, 1 week for CSS.
 //
 function schnell_htaccess_rules() {
-	$result  = '# BEGIN Schnell Optimisation' . PHP_EOL;
-	$result .= '# Response headers' . PHP_EOL;
-	$result .= '<IfModule mod_headers.c>' . PHP_EOL;
-	$result .= "\t" . 'Header set Cache-Control "public"' . PHP_EOL;
-	$result .= "\t" . 'Header set Vary "Accept-Encoding"' . PHP_EOL;
-	$result .= "\t" . 'Header set X-Content-Type-Options "nosniff"' . PHP_EOL;
-	$result .= "\t" . 'Header unset X-Frame-Options' . PHP_EOL;
-	$result .= "\t" . 'Header unset X-Pingback' . PHP_EOL;
-	$result .= "\t" . 'Header unset X-Powered-By' . PHP_EOL;
-	$result .= "\t" . 'Header unset ETag' . PHP_EOL;
-	$result .= '</IfModule>' . PHP_EOL;
-	$result .= '# Leverage browser caching' . PHP_EOL;
+	$result  = '# BEGIN Schnell Cache' . PHP_EOL;
+	$result .= '# Browser caching using Expires headers' . PHP_EOL;
 	$result .= '<IfModule mod_expires.c>' . PHP_EOL;
 	$result .= "\t" . 'ExpiresActive On' . PHP_EOL;
-	$result .= "\t" . 'ExpiresDefault A' . HOUR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Data files are not cached' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/json A0' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/xml A0' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/html A0' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/plain A0' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/xml A0' . PHP_EOL;
-	$result .= "\t" . '# Feeds are cached for 1 hour' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/atom+xml A' . HOUR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/rss+xml A' . HOUR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/x-component A' . HOUR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Scripts are cached for 1 month' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/javascript A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/x-javascript A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/css A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/javascript A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Images are cached for 1 year' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/bmp A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/gif A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/jp2 A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/jpe A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/jpeg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/jpg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/pipeg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/png A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/svg+xml A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/tiff A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/vnd.microsoft.icon A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Icons are cached for 1 year' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/ico A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/ico A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/icon A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/x-ico A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/x-icon A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType text/ico A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Audio files are cached for 1 year' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/basic A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/mid A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/midi A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/mpeg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/ogg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/x-aiff A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/x-mpegurl A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/x-pn-realaudio A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType audio/x-wav A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Video files are cached for 1 year' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/mp4 A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/mpeg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/ogg A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/quicktime A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/webm A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/x-la-asf A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/x-ms-asf A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType video/x-msvideo A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType x-world/x-vrml A' . YEAR_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Fonts are cached for 1 month' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/font-woff A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/vnd.ms-fontobject A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/x-font-ttf A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/x-font-woff A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType font/opentype A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType font/truetype A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . '# Other files are cached for 1 month' . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/pdf A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/smil A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType application/vnd.wap.wbxml A' . MONTH_IN_SECONDS . PHP_EOL;
-	$result .= "\t" . 'ExpiresByType image/vnd.wap.wbmp A' . MONTH_IN_SECONDS . PHP_EOL;
+	$result .= "\t" . '# One week for stylesheet files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType text/css "access plus 1 week"' . PHP_EOL;
+	$result .= "\t" . '# One month for script files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType text/javascript "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType application/javascript "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . '# One month for font files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType font/woff "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType font/woff2 "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType font/ttf "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType font/otf "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/svg+xml "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType application/vnd.ms-fontobject "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . '# One month for portable document files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType application/pdf "access plus 1 month"' . PHP_EOL;
+	$result .= "\t" . '# One year for image files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/jpeg "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/png "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/webp "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/gif "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/svg+xml "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType image/x-icon "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . '# One year for video files' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType video/mp4 "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType video/mpeg "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType video/x-matroska "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType video/avi "access plus 1 year"' . PHP_EOL;
+	$result .= "\t" . '# Others' . PHP_EOL;
+	$result .= "\t" . 'ExpiresByType application/x-shockwave-flash "access plus 1 month"' . PHP_EOL;
 	$result .= '</IfModule>' . PHP_EOL;
-	$result .= '# Enable compression' . PHP_EOL;
+	$result .= '# Browser caching using Cache-Control headers' . PHP_EOL;
+	$result .= '<ifModule mod_headers.c>' . PHP_EOL;
+	$result .= "\t" . '# One week for stylesheet files' . PHP_EOL;
+	$result .= "\t" . '<filesMatch ".(css)$">' . PHP_EOL;
+	$result .= "\t\t" . 'Header set Cache-Control "max-age=604800, public"' . PHP_EOL;
+	$result .= "\t" . '</filesMatch>' . PHP_EOL;
+	$result .= "\t" . '# One month for script files' . PHP_EOL;
+	$result .= "\t" . '<filesMatch ".(js)$">' . PHP_EOL;
+	$result .= "\t\t" . 'Header set Cache-Control "max-age=2592000, public"' . PHP_EOL;
+	$result .= "\t" . '</filesMatch>' . PHP_EOL;
+	$result .= "\t" . '# One month for portable document files' . PHP_EOL;
+	$result .= "\t" . '<filesMatch ".(pdf)$">' . PHP_EOL;
+	$result .= "\t\t" . 'Header set Cache-Control "max-age=2592000, public"' . PHP_EOL;
+	$result .= "\t" . '</filesMatch>' . PHP_EOL;
+	$result .= "\t" . '# One year for image files' . PHP_EOL;
+	$result .= "\t" . '<filesMatch ".(jpg|jpeg|png|webp|gif|svg|ico)$">' . PHP_EOL;
+	$result .= "\t\t" . 'Header set Cache-Control "max-age=31536000, public"' . PHP_EOL;
+	$result .= "\t" . '</filesMatch>' . PHP_EOL;
+	$result .= "\t" . '# One year for video files' . PHP_EOL;
+	$result .= "\t" . '<filesMatch ".(mp4|mpeg|mkv|avi)$">' . PHP_EOL;
+	$result .= "\t\t" . 'Header set Cache-Control "max-age=31536000, public"' . PHP_EOL;
+	$result .= "\t" . '</filesMatch>' . PHP_EOL;
+	$result .= '</ifModule>' . PHP_EOL;
+	$result .= '# Gzip compression' . PHP_EOL;
 	$result .= '<IfModule mod_deflate.c>' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/javascript' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/rss+xml' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/vnd.ms-fontobject' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-font' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-font-opentype' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-font-otf' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-font-truetype' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-font-ttf' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/x-javascript' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/xhtml+xml' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE application/xml' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE font/opentype' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE font/otf' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE font/ttf' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE image/svg+xml' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE image/x-icon' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE text/css' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE text/html' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE text/javascript' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE text/plain' . PHP_EOL;
-	$result .= "\t" . 'AddOutputFilterByType DEFLATE text/xml' . PHP_EOL;
+	$result .= "\t" . '# Active compression' . PHP_EOL;
+	$result .= "\t" . 'SetOutputFilter DEFLATE' . PHP_EOL;
+	$result .= "\t" . '# Force deflate for mangled headers' . PHP_EOL;
+	$result .= "\t" . '<IfModule mod_setenvif.c>' . PHP_EOL;
+	$result .= "\t\t" . '<IfModule mod_headers.c>' . PHP_EOL;
+	$result .= "\t\t\t" . 'SetEnvIfNoCase ^(Accept-EncodXng|X-cept-Encoding|X{15}|~{15}|-{15})$ ^((gzip|deflate)\s*,?\s*)+|[X~-]{4,13}$ HAVE_Accept-Encoding' . PHP_EOL;
+	$result .= "\t\t\t" . 'RequestHeader append Accept-Encoding "gzip,deflate" env=HAVE_Accept-Encoding' . PHP_EOL;
+	$result .= "\t\t\t" . '# Do not compress images and other uncompressible content' . PHP_EOL;
+	$result .= "\t\t\t" . 'SetEnvIfNoCase Request_URI \.(?:gif|jpe?g|png|rar|zip|exe|flv|mov|wma|mp3|avi|swf|mp?g|mp4|webm|webp|pdf)$ no-gzip dont-vary' . PHP_EOL;
+	$result .= "\t\t" . '</IfModule>' . PHP_EOL;
+	$result .= "\t" . '</IfModule>' . PHP_EOL;
+	$result .= "\t" . '# Compress all output labeled with one of the following MIME types' . PHP_EOL;
+	$result .= "\t" . '<IfModule mod_filter.c>' . PHP_EOL;
+	$result .= "\t\t" . 'AddOutputFilterByType DEFLATE application/atom+xml application/javascript application/json application/rss+xml application/vnd.ms-fontobject application/x-font-ttf application/xhtml+xml application/xml font/opentype image/svg+xml image/x-icon text/css text/html text/plain text/x-component text/xml' . PHP_EOL;
+	$result .= "\t" . '</IfModule>' . PHP_EOL;
+	$result .= "\t" . '<IfModule mod_headers.c>' . PHP_EOL;
+	$result .= "\t\t" . 'Header append Vary: Accept-Encoding' . PHP_EOL;
+	$result .= "\t" . '</IfModule>' . PHP_EOL;
 	$result .= '</IfModule>' . PHP_EOL;
-	$result .= '# END Schnell Optimisation' . PHP_EOL;
+	$result .= '# END Schnell Cache' . PHP_EOL;
 	return $result;
 }
 
