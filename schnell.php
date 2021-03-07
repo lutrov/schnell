b74c0fffecd836dc514f78a323eac2ae64030b0f
Ivan Lutrov <lutrov@users.noreply.github.com>
2021-03-07 14:28:14 +1100

    Update schnell.php

--- schnell.php
+++ schnell.php
@@ -2,11 +2,26 @@
 
 /*
 Plugin Name: Schnell
-Description: Improves the speed of your site by enabling client side caching with `Expire` headers, `Cache-Control` headers &amp; output compression. Why this plugin name? Schnell means "quick" in German.
 Plugin URI: https://github.com/lutrov/schnell
+Description: Improves the speed of your site by enabling client side caching with `Expire` headers, `Cache-Control` headers &amp; output compression. Why this plugin name? Schnell means "quick" in German.
+Version: 5.0
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 5.0
+Copyright: 2018, Ivan Lutrov
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA. Also add information on how to
+contact you by electronic and paper mail.
 */
 
 defined('ABSPATH') || die('Ahem.');
